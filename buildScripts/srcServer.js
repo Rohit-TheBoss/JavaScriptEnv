ca4db7474e28bbf73920607d317462b2e2c1842b
Rohit Chauhan <RohitChauhan@Rohits-MacBook-Pro.local>
2018-03-04 23:27:19 -0500

    mock http

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -25,6 +25,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function (req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database.
+  res.json([
+    {"id": 1, "firstName": "Rohit", "lastName": "Chauhan", "email": "rohit.chauhan@gmail.com"},
+    {"id": 2, "firstName": "James", "lastName": "Bang", "email": "james.bang@gmail.com"},
+    {"id": 3, "firstName": "Steven", "lastName": "Mitton", "email": "steven.mitton@gmail.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
